ce6b1e42e28c97949b32255c1a1affa29c56f78b
Oliver Maskery <omaskery@googlemail.com>
2020-04-18 12:42:08 +0100

    Initial commit with player movement

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Transform lookTowards;
+
+    [SerializeField] private Transform lookFrom;
+
+    [Header("Configuration")] [SerializeField]
+    private float cameraHeight;
+    
+    [SerializeField] private float cameraDistance;
+
+    [SerializeField] private float lerpAlpha = 0.1f;
+    
+    void Start()
+    {
+        UpdateCameraPosition(true);
+    }
+
+    void Update()
+    {
+        UpdateCameraPosition();
+    }
+
+    private void UpdateCameraPosition(bool snap=false)
+    {
+        var lookFromPosition = lookFrom.position;
+        var lookTowardsPosition = lookTowards.position;
+
+        var lookFromDirection = (lookFromPosition - lookTowardsPosition).normalized;
+        lookFromPosition = lookTowardsPosition + (lookFromDirection * cameraDistance);
+        
+        var targetPosition = new Vector3(lookFromPosition.x, cameraHeight, lookFromPosition.z);
+        if (!snap)
+        {
+            targetPosition = Vector3.Slerp(transform.position, targetPosition, lerpAlpha);
+        }
+
+        transform.position = targetPosition;
+        transform.LookAt(lookTowards);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("References")] [SerializeField]
+    private Transform coordinatesCentredOn;
+
+    [Header("Ground Check")] [SerializeField]
+    private LayerMask groundCheckMask;
+
+    [SerializeField] private float raycastStartHeight = 1000.0f;
+
+    [Header("Configuration")] [SerializeField]
+    private float horizontalScaling = 0.1f;
+
+    [SerializeField] private float verticalScaling = 1.0f;
+
+    void Start()
+    {
+        _polarCoordinates = polarCordinatesOf(coordinatesCentredOn, transform);
+    }
+
+    void Update()
+    {
+        var currentPosition = transform.position;
+
+        var inputVector = new Vector2(
+            Input.GetAxisRaw("Horizontal") * horizontalScaling,
+            Input.GetAxisRaw("Vertical") * verticalScaling
+        );
+
+        var newPolarCoordinate = _polarCoordinates + inputVector;
+        _polarCoordinates = newPolarCoordinate;
+        var newCartesianCoordinate = cartesianCoordinatesOf(coordinatesCentredOn, newPolarCoordinate);
+
+        var nextPosition = new Vector3(
+            newCartesianCoordinate.x,
+            RaycastGroundHeight(),
+            newCartesianCoordinate.y
+        );
+
+        currentPosition = nextPosition;
+        transform.position = currentPosition;
+    }
+
+    private float RaycastGroundHeight()
+    {
+        var position = transform.position;
+        var raycastOrigin = new Vector3(position.x, raycastStartHeight, position.z);
+
+        if (Physics.Raycast(raycastOrigin, Vector3.down, out var hitInfo, raycastStartHeight * 2, groundCheckMask))
+        {
+            return hitInfo.point.y;
+        }
+
+        return raycastStartHeight;
+    }
+
+    private Vector2 polarCordinatesOf(Transform centre, Transform target)
+    {
+        var targetPosition = target.position;
+        var centrePosition = centre.position;
+        var theta = Mathf.Atan2(
+            targetPosition.z - centrePosition.z,
+            targetPosition.x - centrePosition.x
+        );
+        var radius = (targetPosition - centrePosition).magnitude;
+        return new Vector2(theta, radius);
+    }
+
+    private Vector2 cartesianCoordinatesOf(Transform centre, Vector2 polar)
+    {
+        return new Vector2(
+            Mathf.Cos(polar.x) * polar.y,
+            Mathf.Sin(polar.x) * polar.y
+        );
+    }
+
+    private Vector2 _polarCoordinates;
+}
