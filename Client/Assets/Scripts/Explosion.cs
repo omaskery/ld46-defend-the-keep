fa07e84e48075f366072e64e67ededb445708787
Oliver Maskery <omaskery@googlemail.com>
2020-04-19 20:23:27 +0100

    Add aesthetic secondary explosions

--- Client/Assets/Scripts/Explosion.cs
+++ Client/Assets/Scripts/Explosion.cs
@@ -1,23 +1,51 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Explosion : MonoBehaviour
 {
+    [SerializeField] private Explosion explosionPrefab;
     [SerializeField] private float maxDamage;
     [SerializeField] private float minDamage;
     [SerializeField] private float startRadius;
     [SerializeField] private float endRadius;
     [SerializeField] private float lifetime;
+    [SerializeField] private float secondaryExplosionProbability;
+    [SerializeField] private float secondaryExplosionRange;
+    [SerializeField] private float secondaryExplosionRadiusCoefficient;
     
     private readonly HashSet<GameObject> _damaged = new HashSet<GameObject>();
     private float _timeAlive = 0.0f;
+    private Transform _transform;
     
     void Start()
     {
+        _transform = transform;
         SetRadius(startRadius);
-        Destroy(gameObject, lifetime);
+        Invoke(nameof(OnExpiration), lifetime);
+    }
+
+    private void OnExpiration()
+    {
+        if (Random.value <= secondaryExplosionProbability)
+        {
+            var theta = Random.value * Mathf.PI * 2.0f;
+            var radius = Random.value * secondaryExplosionRange;
+            var position = _transform.position + new Vector3(
+                Mathf.Cos(theta) * radius,
+                0.0f,
+                Mathf.Sin(theta) * radius
+            );
+            
+            var explosion = Instantiate(explosionPrefab, position, _transform.rotation, _transform.parent);
+            explosion.endRadius = endRadius * secondaryExplosionRadiusCoefficient;
+            explosion.startRadius = startRadius * secondaryExplosionRadiusCoefficient;
+            explosion.lifetime *= 0.5f;
+            explosion.maxDamage = 0.0f;
+            explosion.minDamage = 0.0f;
+        }
+        Destroy(gameObject);
     }
 
     private void SetRadius(float radius)
